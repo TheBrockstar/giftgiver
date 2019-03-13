8e418ca742572fd7b42a8466f457cbbe8b0ef46c
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-03-12 21:35:39 -0600

    Implement add-gift Behavior Suite (BDD)

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -14,14 +14,23 @@ describe('App', () => {
     expect(app.state().gifts).toEqual([]);
   });
 
-  it('adds a new gift to `state` when clicking the `add gift` button', () => {
-    app.find('.button-add').simulate('click');
-    expect(app.state().gifts).toEqual([{ id: 1 }]);
-  })
 
-  it('adds a new gift to the rendered list when clicking the `add gift` button.', () => {
-    app.find('.button-add').simulate('click');
-    expect(app.find('.gift-list').children().length).toEqual(2);
+  describe('when clicking the `add-gift` button', () => {
+
+    beforeEach(() => {
+      app.find('.button-add').simulate('click');
+    });
+
+    afterEach(() => {
+      app.setState({ gifts: [] });
+    })
+
+    it('adds a new gift to `state`', () => {
+      expect(app.state().gifts).toEqual([{ id: 1 }]);
+    })
+  
+    it('adds a new gift to the rendered list', () => {
+      expect(app.find('.gift-list').children().length).toEqual(1);
+    });
   });
-
 })

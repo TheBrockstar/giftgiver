162b8a400516b6d3b6be596c3cb6b7bdeb1a5ea5
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-03-12 21:14:24 -0600

    Implement Add Gifts Button

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
   constructor() {
@@ -8,10 +9,23 @@ class App extends Component {
     }
   }
 
+  addGift = _ => {
+    const { gifts } = this.state;
+    
+    const ids = this.state.gifts.map(gift => gift.id);
+
+    const maxId = ids.length > 0 ? Math.max(...ids) : 0;
+
+    gifts.push({ id: maxId + 1 });
+
+    this.setState({ gifts });
+  }
+
   render() {
     return (
       <div>
         <h2>Gift Giver</h2>
+        <Button className="button-add" onClick={this.addGift}>Add Gift</Button>
       </div>
     )
   }
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -12,3 +12,10 @@ it('initializes the `state` with an empty list of gifts', () => {
   expect(app.state().gifts).toEqual([]);
 });
 
+it('adds a new gift to `state` when clicking the `add gift` button', () => {
+  app.find('.button-add').simulate('click');
+  expect(app.state().gifts).toEqual([{ id: 1 }]);
+
+  app.find('.button-add').simulate('click');
+  expect(app.state().gifts).toEqual([{ id: 1 }, { id: 2 }]);
+})

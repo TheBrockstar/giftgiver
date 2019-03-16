4e24851c8de36b0a1aa1ba9f77da009e6df71374
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-03-16 15:16:20 -0600

    Fix Connect Method Syntax. App Renders.

--- lootcheck/src/components/App.test.js
+++ lootcheck/src/components/App.test.js
@@ -3,10 +3,15 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 describe('App', () => {
-const app = shallow(<App />);
+  const app = shallow(<App />);
 
-it('renders properly', () => {
-  expect(app).toMatchSnapshot(app);
-})
+  it('renders properly', () => {
+    expect(app).toMatchSnapshot(app);
+  })
 
+  it('contains a connected wallet component', () => {
+    // console.log(app.debug());
+
+    expect(app.find('Connect(Wallet)').exists()).toBe(true);
+  })
 });

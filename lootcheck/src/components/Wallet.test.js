0be23f4892b8a60a6a3258249dd7dacfce2786c9
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-03-16 16:27:12 -0600

    Implement Full Functionality for Deposit and Withdrawal.

--- lootcheck/src/components/Wallet.test.js
+++ lootcheck/src/components/Wallet.test.js
@@ -3,7 +3,15 @@ import { shallow } from 'enzyme';
 import { Wallet } from './Wallet';
 
 describe('Wallet', () => {
-  const props = { balance: 20 }
+  const mockDeposit = jest.fn();
+  const mockWithdraw = jest.fn();
+
+  const props = { 
+    balance: 20, 
+    deposit: mockDeposit,
+    withdraw: mockWithdraw
+  }
+
   const wallet = shallow(<Wallet {...props} />);
 
    it('renders properly', () => {
@@ -13,4 +21,37 @@ describe('Wallet', () => {
    it('displays the balance from props', () => {
      expect(wallet.find('.balance').text()).toEqual('Wallet Balance: 20');
    });
+
+   it('renders an input to deposit into or withdraw from the balance', () => {
+     expect(wallet.find('.input-wallet').exists()).toBe(true);
+   });
+
+   describe('when the user types into the wallet input', () => {
+    const userBalance = '25';
+
+    beforeEach(() => {
+      wallet.find('.input-wallet').simulate('change', { target: { value: userBalance }});
+    });
+
+    it('updates the local wallet balance in `state` and converts it to a number', () => {
+      expect(wallet.state().balance).toEqual(parseInt(userBalance, 10));
+    });
+    
+    describe('and the user wants to make a deposit', () => {
+      beforeEach(() => wallet.find('.button-deposit').simulate('click'));
+
+      it('dispatch the `deposit()` it receives from props with local balance', () => {
+        expect(mockDeposit).toHaveBeenCalledWith(parseInt(userBalance, 10));
+      });
+    });
+
+    describe('and the user wants to make a withdrawal', () => {
+      beforeEach(() => wallet.find('.button-withdraw').simulate('click'));
+      
+      it('dispatch the `withdraw()` it receives from props with the local balance', () => {
+        expect(mockWithdraw).toHaveBeenCalledWith(parseInt(userBalance, 10));
+      });
+    });
+  });
 });
+
